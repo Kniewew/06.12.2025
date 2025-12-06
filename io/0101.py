e5f66f69fb0054552e41f76ed6fc1ac268159b4c
Lable <likyfa19diri@gmail.com>
2025-12-06 22:07:10 +0300

    Add files via upload

---
+++ io/0101.py
@@ -0,0 +1,86 @@
+import random
+
+xle = [
+    [" "," "," "],
+    [" "," "," "],
+    [" "," "," "]
+]
+def show(xle):
+    """выводит игровое поле"""
+    for i in xle:
+        print(i, end=" ")
+        print(end="\n")
+
+def focus(xle):
+    """ход добавляет ход игрока"""
+    try:
+        x = int(input("ваш ход х = "))
+        y = int(input("ваш ход у = "))
+    except:
+        print("error")
+        pass
+    if x <=3 and y <=3:
+        if xle[y][x] != "X" and xle[y][x] != "O":
+            xle[y][x] = "X"
+    else:
+        print("нет такого хода или он занят")
+
+def prompt(xle):
+    """машина делает ход"""
+    xm = random.randint(0,2)
+    ym = random.randint(0,2)
+    while True:
+        if xle[ym][xm] != "X" and xle[ym][xm] != "O":
+            xle[ym][xm] = "O"
+            break
+        else:
+            continue
+def win_player(a):
+    """определяет победителя"""
+    for i in range(3):
+        a = 0
+        for j in range(3):
+            if xle[i][j] == "X" or xle[j][i] == "X":
+                a += 1
+            if xle[0][0] == "X" and xle[1][1] == "X" and xle[2][2] == "X":
+                a += 3
+            if xle[0][2] =="X" and xle[1][1] == "X" and xle[2][0] == "X":
+                a += 3
+            if a == 3:
+                return a
+def win_machine(a):
+    """определяет победителя"""
+    for i in range(3):
+        a = 0
+        for j in range(3):
+            if xle[i][j] == "X" or xle[j][i] == "X":
+                a += 1
+            if xle[0][0] == "X" and xle[1][1] == "X" and xle[2][2] == "X":
+                a += 3
+            if xle[0][2] =="X" and xle[1][1] == "X" and xle[2][0] == "X":
+                a += 3
+            if a == 3:
+                return a
+
+while True:
+    show(xle)
+    focus(xle)
+    if win_player("X"):
+        print("победил игрок")
+        show(xle)
+        break
+    show(xle)
+    prompt(xle)
+    if win_player("O"):
+        print("победил компютер")
+        show(xle)
+        break
+
+
+
+
+
+
+
+
+
